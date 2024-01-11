43b22f8e8e1c19eaf7e763fb833e0873539ac159
Markus Wallerberger <markus.wallerberger@tuwien.ac.at>
2024-01-11 15:10:26 +0100

    Make VersionFromHeader nicer.
    I still don't like CMake, but that should at least make it a bit
    nicer to use.

--- config/cmake/VersionFromHeader.cmake
+++ config/cmake/VersionFromHeader.cmake
@@ -4,24 +4,44 @@
 # SPDX-License-Identifier: MIT
 
 # Function to extract version string from header file
-function(version_from_header version filename
-         macro_major macro_minor macro_patch)
-    file(READ "${filename}" header)
+function(version_from_header)
+    # parse arguments
+    set(options)
+    set(one_value_args VERSION HEADER)
+    set(multi_value_args MACROS)
+    cmake_parse_arguments(_ARG "${options}" "${one_value_args}"
+                          "${multi_value_args}" ${ARGN})
 
-    string(REGEX MATCH
-        "define[ \t]+${macro_major}[ \t]+([0-9]+)"
-        match "${header}")
-    set(_version "${CMAKE_MATCH_1}")
+    # These are required
+    if (NOT _ARG_VERSION)
+        message(FATAL_ERROR "VERSION is required")
+    endif()
+    if (NOT _ARG_HEADER)
+        message(FATAL_ERROR "HEADER is required")
+    endif()
+    if (NOT _ARG_MACROS)
+        message(FATAL_ERROR "MACROS is required")
+    endif()
 
-    string(REGEX MATCH
-        "define[ \t]+${macro_minor}[ \t]+([0-9]+)"
-        match "${header}")
-    set(_version "${_version}.${CMAKE_MATCH_1}")
+    # read in the file
+    file(READ "${_ARG_HEADER}" header)
 
-    string(REGEX MATCH
-        "define[ \t]+${macro_patch}[ \t]+([0-9]+)"
-        match "${header}")
-    set(_version "${_version}.${CMAKE_MATCH_1}")
+    # build up version string from macros
+    set(version "")
+    foreach(macro_name IN LISTS _ARG_MACROS)
+        if (header MATCHES "define[ \t]+${macro_name}[ \t]+([0-9]+)")
+            set(chunk "${CMAKE_MATCH_1}")
+            if (version STREQUAL "")
+                set(version "${chunk}")
+            else()
+                set(version "${version}.${chunk}")
+            endif()
+        else()
+            message(SEND_ERROR "${macro_name} not #defined in ${_ARG_HEADER}")
+            return()
+        endif()
+    endforeach()
 
-    set("${version}" "${_version}" PARENT_SCOPE)
+    # export back to parent scope ("pass by reference")
+    set("${_ARG_VERSION}" "${version}" PARENT_SCOPE)
 endfunction()
