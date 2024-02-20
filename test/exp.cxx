a752bcf42ad287fecac26ea40ea2aeb87152910a
Markus Wallerberger <markus.wallerberger@tuwien.ac.at>
2024-02-20 11:30:29 +0100

    Tighten test limits.

--- test/exp.cxx
+++ test/exp.cxx
@@ -27,23 +27,24 @@ TEST_CASE("pow", "[fn]")
 
 TEST_CASE("exp", "[exp]")
 {
-    CMP_UNARY(exp, 0.0, 1e-31);
-    CMP_UNARY(exp, 1.0, 1e-31);
-    CMP_UNARY(exp, 1e-4, 1e-31);
+    const double ulp = 2.4651903288156619e-32;
+    CMP_UNARY(exp, 0.0, 1 * ulp);
+    CMP_UNARY(exp, 1.0, 1 * ulp);
+    CMP_UNARY(exp, 1e-4, 1.0 * ulp);
 
     // Small values shall be very accurate
     DDouble x = 0.25;
     while ((x *= 0.9) > 1e-290) {
-        CMP_UNARY(exp, x, 5e-32);
-        CMP_UNARY(exp, -x, 5e-32);
+        CMP_UNARY(exp, x, 1.0 * ulp);
+        CMP_UNARY(exp, -x, 1.0 * ulp);
     }
 
     // Larger, less so, but let's still strive for 1 ulps
     x = 0.125;
     while ((x *= 1.0041) < 708.0) {
-        CMP_UNARY(exp, x, 5e-32);
+        CMP_UNARY(exp, x, 2.0 * ulp);
         if (x < 670)
-            CMP_UNARY(exp, -x, 5e-32);
+            CMP_UNARY(exp, -x, 2.0 * ulp);
     }
 
     REQUIRE(exp(DDouble(-1000)) == 0);
@@ -51,58 +52,61 @@ TEST_CASE("exp", "[exp]")
 
 TEST_CASE("expm1", "[exp]")
 {
-    CMP_UNARY(expm1, 0.0, 1e-31);
-    CMP_UNARY(expm1, 1.0, 1e-31);
-    CMP_UNARY(expm1, -1000, 1e-31);
+    const double ulp = 2.4651903288156619e-32;
+    CMP_UNARY(expm1, 0.0, 1.0 * ulp);
+    CMP_UNARY(expm1, 1.0, 1.0 * ulp);
+    CMP_UNARY(expm1, -1000, 1.0 * ulp);
 
     // Small values shall be very accurate
     DDouble x = 0.25;
     while ((x *= 0.9) > 1e-290) {
-        CMP_UNARY(expm1, x, 5e-32);
-        CMP_UNARY(expm1, -x, 5e-32);
+        CMP_UNARY(expm1, x, 1.5 * ulp);
+        CMP_UNARY(expm1, -x, 1.5 * ulp);
     }
 
     // Larger, less so, but let's still strive for 1 ulps
     x = 0.125;
     while ((x *= 1.02) < 708.0) {
-        CMP_UNARY(expm1, x, 8e-32);
+        CMP_UNARY(expm1, x, 2.0 * ulp);
         if (x < 670)
-            CMP_UNARY(expm1, -x, 8e-32);
+            CMP_UNARY(expm1, -x, 2.0 * ulp);
     }
 }
 
 TEST_CASE("log", "[exp]")
 {
-    CMP_UNARY(log, 1.0, 1e-31);
-    CMP_UNARY(log, 3.0, 1e-31);
+    const double ulp = 2.4651903288156619e-32;
+    CMP_UNARY(log, 1.0, 1.0 * ulp);
+    CMP_UNARY(log, 3.0, 1.0 * ulp);
 
     DDouble x = 1.;
     while ((x *= 1.13) < 1e300) {
-        CMP_UNARY(log, x, 1e-31);
+        CMP_UNARY(log, x, 1.0 * ulp);
     }
 
     x = 1.;
     while ((x *= 0.95) > 1e-290) {
-        CMP_UNARY(log, x, 1e-31);
+        CMP_UNARY(log, x, 1.0 * ulp);
     }
 }
 
 TEST_CASE("log1p", "[exp]")
 {
-    CMP_UNARY(log, 1.0, 1e-31);
-    CMP_UNARY(log, 3.0, 1e-31);
+    const double ulp = 2.4651903288156619e-32;
+    CMP_UNARY(log, 1.0, 1.0 * ulp);
+    CMP_UNARY(log, 3.0, 1.0 * ulp);
 
     DDouble x = 1.;
     while ((x *= 1.13) < 1e300) {
-        CMP_UNARY(log1p, x, 1e-31);
+        CMP_UNARY(log1p, x, 1.0 * ulp);
     }
 
     x = 1.;
     while ((x *= 0.92) > 1e-290) {
-        CMP_UNARY(log1p, x, 1e-31);
+        CMP_UNARY(log1p, x, 1.5 * ulp);
     }
     x = -.9999999;
     while ((x *= 0.92) > 1e-290) {
-        CMP_UNARY(log1p, x, 1e-31);
+        CMP_UNARY(log1p, x, 1.0 * ulp);
     }
 }
