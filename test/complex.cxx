0043257ff1dd92b7078952f37412687c24ddeef2
Markus Wallerberger <markus.wallerberger@tuwien.ac.at>
2024-04-09 17:48:55 +0200

    Complex numbers.

---
+++ test/complex.cxx
@@ -0,0 +1,17 @@
+/* Tests
+ *
+ * Copyright (C) 2023 Markus Wallerberger and others
+ * SPDX-License-Identifier: MIT
+ */
+#include <catch2/catch_test_macros.hpp>
+#include <complex>
+#include <iostream>
+#include "xprec/ddouble.h"
+
+using xprec::DDouble;
+
+TEST_CASE("simple", "[complex]")
+{
+    std::complex<DDouble> a = {DDouble(0.0), DDouble(3.0)};
+    std::cout << (a + DDouble(1.0)) / (a + DDouble(2.0));
+}
