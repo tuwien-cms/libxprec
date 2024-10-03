af25821b741df1113c2aa7fb8c404d2f13f69414
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2024-10-03 14:16:06 +0200

    Test case found by Hiroshi.

--- test/eigen.cxx
+++ test/eigen.cxx
@@ -22,3 +22,16 @@ TEST_CASE("eigenvalues", "[eigen]")
     Eigen::SelfAdjointEigenSolver<decltype(A)> A_eig(A);
     // TODO
 }
+
+TEST_CASE("prod", "[eigen]")
+{
+    using Eigen::Dynamic;
+
+    Eigen::Matrix<DDouble, Dynamic, Dynamic> A(21, 21);
+    for (int j = 0; j != A.cols(); ++j)
+        for (int i = 0; i != A.rows(); ++i)
+            A(i, j) = xprec::reciprocal(xprec::ExDouble(i + j + 1));
+
+    auto A2 = A * A;
+    REQUIRE(A2.sum() > 0);
+}
