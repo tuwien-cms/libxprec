6d720b64e0ca5bba9278baa8178e53cd9eb176b6
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2024-10-03 14:24:13 +0200

    Hiroshi's SVD test case.

--- test/eigen.cxx
+++ test/eigen.cxx
@@ -10,10 +10,10 @@
 
 #include <Eigen/Dense>
 
+using Eigen::Dynamic;
+
 TEST_CASE("eigenvalues", "[eigen]")
 {
-    using Eigen::Dynamic;
-
     Eigen::Matrix<DDouble, Dynamic, Dynamic> A(21, 21);
     for (int j = 0; j != A.cols(); ++j)
         for (int i = 0; i != A.rows(); ++i)
@@ -25,8 +25,6 @@ TEST_CASE("eigenvalues", "[eigen]")
 
 TEST_CASE("prod", "[eigen]")
 {
-    using Eigen::Dynamic;
-
     Eigen::Matrix<DDouble, Dynamic, Dynamic> A(21, 21);
     for (int j = 0; j != A.cols(); ++j)
         for (int i = 0; i != A.rows(); ++i)
@@ -34,4 +32,24 @@ TEST_CASE("prod", "[eigen]")
 
     auto A2 = A * A;
     REQUIRE(A2.sum() > 0);
+}
+
+TEST_CASE("singular values", "[eigen]")
+{
+    Eigen::Matrix<DDouble, Dynamic, Dynamic> A(21, 21);
+    for (int j = 0; j != A.cols(); ++j)
+        for (int i = 0; i != A.rows(); ++i)
+            A(i, j) = xprec::reciprocal(xprec::ExDouble(i + j + 1));
+
+    Eigen::JacobiSVD<decltype(A)> svd;
+    svd.compute(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
+
+    auto U = svd.matrixU();
+    auto V = svd.matrixV();
+    auto S = svd.singularValues().asDiagonal();
+    auto A_reconstructed = U * S * V.transpose();
+
+    auto diff = A - A_reconstructed;
+    auto reconstruction_error = diff.norm();
+    REQUIRE(reconstruction_error < 1e-29);
 }
