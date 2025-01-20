e28fbee60aaa377864061068228d36817b0d5e27
Markus Wallerberger <markus.wallerberger@tuwien.ac.at>
2025-01-20 13:31:26 +0100

    Try unit tests like this.

--- include/xprec/ddouble-header-only.hpp
+++ include/xprec/ddouble-header-only.hpp
@@ -14,6 +14,7 @@
 // The strategy for header only use of ddouble is pretty simple:
 // conditionally inline globally defined functions and include the cxx files
 // directly.
+#undef XPREC_API_EXPORT
 #define XPREC_API_EXPORT static
 
 #include "../../src/circular.cpp"
